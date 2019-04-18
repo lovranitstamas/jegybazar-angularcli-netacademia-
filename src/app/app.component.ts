fb47a625dba6d90065decb2487e62dc94a9d0677
lovranitslaptop <tamas198601@gmail.com>
2019-04-18 17:42:53 +0200

    Return to the next commit -> Repeat part of the copy of the previous commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,6 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'jegybazarnewversion';
-  events = ['sziget', 'volt', 'efot', 'fazen'];
 
-  show = false;
-  klikk = true;
-
-  inputContent = 'Basic value';
-
-
-  toggleBackgroundColor() {
-    this.show = !this.show;
-    this.inputContent = 'macska';
-  }
-
-  demo(ev: MouseEvent) {
-    console.log(ev.screenX);
-  }
 }
+
