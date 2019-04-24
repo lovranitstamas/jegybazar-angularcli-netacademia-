cec0be9d0f7ab5db46cc15e287495bbde68e71aa
lovranitslaptop <tamas198601@gmail.com>
2019-04-24 22:24:38 +0200

    Logout and registration with exampleUser

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../shared/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,10 +9,13 @@ import {Component, OnInit} from '@angular/core';
 export class NavbarComponent implements OnInit {
   public isCollapsed = false;
 
-  constructor() {
+  constructor(public userService: UserService) {
   }
 
   ngOnInit() {
   }
 
+  logout() {
+    this.userService.logout();
+  }
 }
--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -14,13 +14,30 @@ export class UserService {
 
   login(email: string, password: string): boolean {
     if (email === 'angular' && password === 'angular') {
-
       this._user = new UserModel(UserModel.exampleUser);
       this.isLoggedIn = true;
       this._router.navigate(['/user']);
-
     }
 
+    console.log('Login: ' + this.isLoggedIn);
     return false;
+  }
+
+  register(param?: UserModel) {
+    if (param) {
+      this._user = new UserModel(param);
+    } else {
+      this._user = new UserModel(UserModel.exampleUser);
+    }
+    this.isLoggedIn = true;
+    console.log('Login: ' + this.isLoggedIn);
+    this._router.navigate(['/user']);
+  }
+
+  logout() {
+    this._user = new UserModel();
+    this.isLoggedIn = false;
+    console.log('Login: ' + this.isLoggedIn);
+    this._router.navigate(['/home']);
   }
 }
