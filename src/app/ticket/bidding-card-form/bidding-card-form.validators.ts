c6c7392ec130ff0dd820da43716482078968097e
lovranitslaptop <tamas198601@gmail.com>
2019-06-02 13:51:01 +0200

    Bug fix - Parsing minimum bid

--- src/app/ticket/bidding-card-form/bidding-card-form.validators.ts
+++ src/app/ticket/bidding-card-form/bidding-card-form.validators.ts
@@ -5,7 +5,7 @@ import { TicketModel } from '../../shared/ticket-model';
 export function bidMinimumValidator(getTicketFn: () => TicketModel) {
   return function(formControl: FormControl){
     const ticket = getTicketFn();
-    const minimumValue = ticket.currentBid + ticket.bidStep; 
+    const minimumValue = +ticket.currentBid + +ticket.bidStep; 
 
     if(parseInt(formControl.value, 0) < minimumValue){
       return {
