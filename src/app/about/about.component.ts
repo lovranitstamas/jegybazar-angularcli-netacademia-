dca8be365709a6eb8c2436ef9249f3b43cfe69eb
lovranitslaptop <tamas198601@gmail.com>
2019-08-16 00:34:41 +0200

    About modify to detach mode

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,17 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component} from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit {
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    this.cdr.detach();
+  }
 
 }
