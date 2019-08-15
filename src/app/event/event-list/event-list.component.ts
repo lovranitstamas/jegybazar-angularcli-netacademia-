b670a185591938d7a4eb2f78e9cdcee418986042
lovranitslaptop <tamas198601@gmail.com>
2019-08-16 00:48:53 +0200

    Event list modify to detach mode

--- src/app/event/event-list/event-list.component.ts
+++ src/app/event/event-list/event-list.component.ts
@@ -1,23 +1,61 @@
-import {AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {EventService} from '../event.service';
-import {EventModel} from '../../shared/event-model';
 import {UserService} from '../../shared/user.service';
-import {BehaviorSubject, fromEvent, Observable} from 'rxjs';
+import {BehaviorSubject, fromEvent, Subscription} from 'rxjs';
 import {delay, distinctUntilChanged, flatMap, map} from 'rxjs/operators';
+import {EventModel} from '../../shared/event-model';
 
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
   styleUrls: ['./event-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EventListComponent implements OnInit, AfterViewInit {
-  public events$: Observable<EventModel[]>;
+export class EventListComponent implements OnInit, AfterViewInit, OnDestroy {
+  // public events$: Observable<EventModel[]>;
+  events: EventModel[];
+  isLoggedIn: boolean;
   @ViewChild('searchInput') searchInput: ElementRef;
   private filteredText$ = new BehaviorSubject<string>(null);
+  private _eventsSubscription: Subscription;
+  private isLoggedInSubscription: Subscription;
 
   constructor(private _eventService: EventService,
-              public userService: UserService) {
+              userService: UserService,
+              private cdr: ChangeDetectorRef
+  ) {
+    this.isLoggedInSubscription = userService.isLoggedIn$.subscribe(
+      isLoggedIn => this.isLoggedIn = isLoggedIn
+    );
+  }
+
+  ngOnInit() {
+    this._eventsSubscription = this._eventService.getAllEvents().pipe(
+      flatMap(
+        events => {
+          return this.filteredText$.pipe(
+            map(
+              filterText => {
+                if (filterText === null) {
+                  return events;
+                } else {
+                  return events.filter(
+                    event => {
+                      return event.name.toLowerCase().indexOf(filterText.toLowerCase()) > -1;
+                    }
+                  );
+                }
+              }
+            )
+          );
+        }
+      )
+    ).subscribe(
+      events => {
+        this.events = events;
+        this.cdr.detectChanges();
+      }
+    );
   }
 
   ngAfterViewInit(): void {
@@ -39,30 +77,13 @@ export class EventListComponent implements OnInit, AfterViewInit {
           this.filteredText$.next(text);
         }
       );
+
+    this.cdr.detach();
   }
 
-  ngOnInit() {
-    this.events$ = this._eventService.getAllEvents().pipe(
-      flatMap(
-        events => {
-          return this.filteredText$.pipe(
-            map(
-              filterText => {
-                if (filterText === null) {
-                  return events;
-                } else {
-                  return events.filter(
-                    event => {
-                      return event.name.toLowerCase().indexOf(filterText.toLowerCase()) > -1;
-                    }
-                  );
-                }
-              }
-            )
-          );
-        }
-      )
-    );
+  ngOnDestroy(): void {
+    this._eventsSubscription.unsubscribe();
+    this.isLoggedInSubscription.unsubscribe();
   }
 
 }
